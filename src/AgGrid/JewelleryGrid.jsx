93c0fb6a1aabc7b4a65417c855f62d85abc03078
Dhruv <dhruvrg2003@gmail.com>
2023-12-25 12:05:47 +0530

    added dark mode for table and file utility

--- src/AgGrid/JewelleryGrid.jsx
+++ src/AgGrid/JewelleryGrid.jsx
@@ -1,22 +1,30 @@
-import React, { useState } from 'react';
-import { AgGridReact } from 'ag-grid-react'; // React Grid Logic
-import 'ag-grid-community/styles/ag-grid.css'; // Core CSS
-import 'ag-grid-community/styles/ag-theme-quartz.css'; // Theme
-import 'ag-grid-enterprise'
-import { JwelleryColsDefs, JwelleryData } from '../assets/JewelleryData';
+import React, { useState } from "react";
+import { AgGridReact } from "ag-grid-react"; // React Grid Logic
+import "ag-grid-community/styles/ag-grid.css"; // Core CSS
+import "ag-grid-community/styles/ag-theme-quartz.css"; // Theme
+import "ag-grid-enterprise";
+import { JwelleryColsDefs, JwelleryData } from "../assets/JewelleryData";
+import { useTheme } from "@mui/material";
 
 const JewelleryGrid = () => {
   const [rowData, setRowData] = useState(JwelleryData);
   const [colDefs, setColDefs] = useState(JwelleryColsDefs);
+  const theme = useTheme();
 
-  
   return (
     <>
-      <div className="ag-theme-quartz-dark" style={{ height: 500 }}>
+      <div
+        className={
+          theme.palette.mode === "light"
+            ? "ag-theme-quartz"
+            : "ag-theme-quartz-dark"
+        }
+        style={{ height: 500 }}
+      >
         <AgGridReact
           rowData={rowData}
           columnDefs={colDefs}
-          rowSelection={'multiple'}
+          rowSelection={"multiple"}
           rowMultiSelectWithClick={true}
           rowDragManaged={true}
         />
